a71f1a19abc848a6d996b7afe2eaf981d85b5d02
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2023-12-12 21:52:41 +0530

    this file is about the expressions in the javascript

---
+++ expression .js
@@ -0,0 +1,55 @@
+// this tutorial is about expressions and conditional expression
+let a = 5;
+let b = 10;
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a*b =",a*b)
+console.log("a/b =",a/b)
+console.log("a**b =",a**b)
+console.log("a%b =",a%b)
+console.log("a =",a,"b =",b)
+console.log("a++ =",a++)
+console.log("a =",a)
+console.log("a-- =",a--)
+console.log("a =",a)
+console.log("++a =",++a)
+console.log("a =",a)
+console.log("--b =",--b)
+console.log("b++ =",b++)
+console.log("b =",b)
+console.log("b-- =",b--)
+console.log("b =",b)
+console.log("++b =",++b)
+console.log("b =",b)
+console.log("--b =",--b)
+console.log("a =",a,"b =",b)
+
+// now assingment operator
+console.log("a=b")
+console.log("a+=b = a=a+b",a+=b)
+console.log("a-=b = a=a-b",a-=b)
+console.log("a*=b = a=a*b",a*=b)
+console.log("a/=b = a=a/b",a/=b)
+console.log("a%=b = a=a%b",a%=b)
+console.log("a =",a,"b =",b)
+
+// comparsion operator
+console.log("== ; equal to",a == b)
+console.log("!= not equal to ",a != b)
+console.log("=== equal and type of ", a === "b")
+console.log("!== not equal to and type of ", a !== b)
+console.log(a,b)
+
+// logical expression
+
+console.log("&& or and ", a>b && a == (a+"3"))
+console.log("|| or OR ", a>b || a == 6)
+console.log(!false)
+console.log(!true)
+let Switch = false;
+// Switch = false ;
+/* 
+this is a multiline comments
+it is used for bigger comments */
+
+
