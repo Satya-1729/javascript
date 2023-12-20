3af027481b1ce385fc7c28290adc8e0206795e2d
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2023-12-20 18:56:54 +0530

    this is about conditional statements

---
+++ conditional statements.js
@@ -0,0 +1,42 @@
+let a = prompt("enter your name");
+a = number.parseint(a)
+console.log(typeof a)
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+let a = prompt("hey whats your age ");
+a = Number.parseInt(a) //converting string to number 
+if (a<0){
+    console.log("you are not verified user")
+    
+}
+else if(a<10){
+    console.log("you are still underage")
+}
+else if (a>18 && a<80){
+    console.log("you can drive the car")
+}
+else{
+    console.log("you are underage")
+}
+console.log(a)
+
+// switching statment 
+const b = prompt("enter your name ")
+switch (b){
+    case "krishna":
+        console.log("this is my friend ")
+        break
+    case "ajay"&&"chitransh":
+        console.log("these two are also my firend")
+        break
+    case "satya":
+        console.log("you this is me ")
+        break
+    default:
+        console.log("sorry we cannot find anyone with this name")
+        break
+        
+
+}
