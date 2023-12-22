2ded80a33d141c75aa20a65dd8de82e7fff2f336
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2023-12-22 19:22:37 +0530

    this is about fn

---
+++ functions.js
@@ -0,0 +1,13 @@
+// functions in the javascript 
+const add = (p,q)=>{
+    console.log("two numbers that are given by users are as follow",p,q)
+    return p+q;
+}
+console.log("addition of two numbers is",add(6,120))
+// old_style_deinfinig_the_function_in_the_javascript_is_as_follow
+function avg(a,b){
+    return (a+b)/2;
+}
+console.log("Average of two numbers",avg(8,10))
+
+
