618f9220c9b791d49261c27d30d8c9b394623e96
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-02-16 13:40:50 +0530

    add these files

---
+++ script5.js
@@ -0,0 +1,9 @@
+console.log(document.body.lastChild)
+console.log(document.body.firstChild)
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
+let a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
+console.log(a.firstChild)
