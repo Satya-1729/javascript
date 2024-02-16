6753ef926d639e3b4def426003e7aa421d6686cb
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-02-16 22:47:22 +0530

    new files

---
+++ script6.js
@@ -0,0 +1,7 @@
+const changebg =()=>{
+    document.body.firstElementChild.style.background = "red"
+}
+let a = document.body
+console.log("the first child of a ",a.firstChild)
+console.log("the first element of  child a ",a.firstElementChild)
+console.log("the last element of  child a ",a.lastElementChild)
