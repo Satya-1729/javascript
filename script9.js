6c4eee71a5a449bc1f20af09d05d6e7dffea33c6
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-02-19 21:31:44 +0530

    matches,closets,contains

---
+++ script9.js
@@ -0,0 +1,16 @@
+let id1 = document.getElementById("id1")
+let sp1 = document.getElementById("sp1")
+
+console.log(id1)
+console.log(id1.matches(".class"))
+console.log(id1.matches(".box"))
+console.log(sp1.matches("#sp1"))
+console.log(id1.closest("#id1"))
+console.log(id1.closest("#sp1"))
+console.log(sp1.closest("#id1"))
+console.log(id1.contains(sp1))
+
+
+
+
+
