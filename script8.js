03e159f142052180702404f92c8c72fe9bd558e6
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-02-19 18:19:36 +0530

    adding

---
+++ script8.js
@@ -0,0 +1,8 @@
+// let t = document.getELementById("card-title")
+let g = document.querySelectorAll(".card-title")
+console.log(g)
+let m = document.getElementById("firstcard")
+m.style.color = "blue"
+console.log(m)
+let  ctitle = document.getElementsByClassName("card-title")[0]
+// ctitle.style.color="red"
