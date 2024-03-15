3f5695f81f6ac3dcfe421a1dcaf7d3cf4dab70e7
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-03-15 21:48:49 +0530

    error handling

---
+++ error_handling.js
@@ -0,0 +1,30 @@
+setTimeout(()=>{
+    console.log("hacking your id......")
+},1000)
+/*try and catch are the synchronous code they are not scheduled 
+let example*/
+// try{
+//     setTimeout(()=>{
+//         console.log(hacking your id......)
+//     },100)
+//     // it will not work as above mentioned
+
+// }
+try{
+    console.log("we are trying error handling")
+    console.log(harry)
+}
+catch(error){
+    console.log("balle balle " + error)
+
+}
+setTimeout(()=>{
+    console.log("hacking your id and password......")
+},2000)
+setTimeout(()=>{
+    console.log("please wait while loading your details......")
+},3000)
+setTimeout(()=>{
+    console.log("hello ray your id is ray_123 and password is raychutiya......")
+},4000)
+
