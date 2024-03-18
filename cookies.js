2273118d35491b57bd72ee7fec735fa66af64e8f
Satya-1729 <148767628+Satya-1729@users.noreply.github.com>
2024-03-18 12:06:28 +0530

    somechanges

--- cookies.js
+++ cookies.js
@@ -1,8 +1,10 @@
 console.log(document.cookies)
-document.cookie = 'name = satya'
-document.cookie = 'name2 = satyasoni'
+document.cookie = 'name = satya'//these are set calls
+document.cookie = 'name2 = satyasoni'//these are set calls
 console.log(document.cookie)
 let key = prompt("enter your key ")
 let value = prompt("enter your value ")
-document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`//these are set calls
 console.log(document.cookie)
+decodeURIComponent('satya%3B%3B%3B%3D%3D')
+document.cookie = 'user=bhai; path = /a; expires = Sunday, 17 March 2024, 16:59:58 GMT'
